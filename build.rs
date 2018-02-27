f6b760b57c249490377c134d605d739c46ec30f4
Nicholas Rishel <nick@accups.com>
2018-02-27 18:49:39 -0500

    Modified to clone and compile libusb in addition to hidapi. Only supports Linux.
    Signed-off-by: Nicholas Rishel <nick@accups.com>

--- build.rs
+++ build.rs
@@ -6,13 +6,15 @@ use std::io;
 use std::path::PathBuf;
 use std::process::Command;
 
-fn main() {
-	if env::var("CARGO_FEATURE_BUILD").is_err() {
-		return;
-	}
+static LIBUSB_DIR: &'static str = "libusb-1.0";
+static HIDAPI_DIR: &'static str = "hidapi";
 
-	fetch().expect("failed to checkout hidapi sources, internet connection and git are needed");
-	build().expect("failed to build hidapi sources");
+fn main() {
+	clone_libusb().expect("failed to checkout libusb sources, internet connection and git are needed");
+	build_libusb().expect("failed to build libusb sources");
+
+	clone_hidapi().expect("failed to checkout hidapi sources, internet connection and git are needed");
+	build_hidapi().expect("failed to build hidapi sources");
 
 	println!("cargo:rustc-link-search=native={}", output().to_string_lossy());
 }
@@ -21,73 +23,105 @@ fn output() -> PathBuf {
 	PathBuf::from(env::var("OUT_DIR").unwrap())
 }
 
-fn source() -> PathBuf {
-	if let Ok(path) = env::var("HIDAPI_PATH") {
-		path.into()
-	}
-	else {
-		output().join("hidapi")
-	}
+fn source(lib: &str) -> PathBuf {
+	output().join(lib)
 }
 
-fn fetch() -> io::Result<()> {
-	if env::var("HIDAPI_PATH").is_ok() {
-		return Ok(());
-	}
-
+fn clone_libusb() -> io::Result<()> {
 	Command::new("git")
 		.current_dir(&output())
 		.arg("clone")
-		.arg("https://github.com/signal11/hidapi.git")
-		.arg("hidapi")
+		.arg("https://github.com/AccuPS/libusb")
+		.arg(LIBUSB_DIR)
+		.arg("--branch")
+		.arg("android")
+		.status()?;
+
+	Ok(())
+}
+
+fn clone_hidapi() -> io::Result<()> {
+	Command::new("git")
+		.current_dir(&output())
+		.arg("clone")
+		.arg("https://github.com/AccuPS/hidapi")
+		.arg(HIDAPI_DIR)
 		.status()?;
 
 	Ok(())
 }
 
 #[cfg(target_os = "linux")]
-fn build() -> io::Result<()> {
+fn build_libusb() -> io::Result<()> {
 	let mut build = cc::Build::new();
 
-	build.file(source().join("libusb/hid.c"));
-	build.include(source().join("hidapi"));
-	build.static_flag(true);
+	let sources = vec![
+		source(LIBUSB_DIR).join("libusb/core.c"),
+		source(LIBUSB_DIR).join("libusb/descriptor.c"),
+		source(LIBUSB_DIR).join("libusb/hotplug.c"),
+		source(LIBUSB_DIR).join("libusb/io.c"),
+		source(LIBUSB_DIR).join("libusb/sync.c"),
+		source(LIBUSB_DIR).join("libusb/strerror.c"),
+		source(LIBUSB_DIR).join("libusb/os/linux_usbfs.c"),
+		source(LIBUSB_DIR).join("libusb/os/poll_posix.c"),
+		source(LIBUSB_DIR).join("libusb/os/threads_posix.c"),
+		source(LIBUSB_DIR).join("libusb/os/linux_netlink.c")
+	];
 
-	for path in pkg_config::find_library("libusb-1.0").unwrap().include_paths {
-		build.include(path.to_str().unwrap());
+	build.files(sources);
+	build.include(source(LIBUSB_DIR).join("android"));
+	build.include(source(LIBUSB_DIR).join("libusb"));
+	build.include(source(LIBUSB_DIR).join("libusb/os"));
+	if cfg!(target_os = "linux") {
+		build.define("_GNU_SOURCE", None);
 	}
+	build.shared_flag(true);
 
-	build.compile("libhidapi-libusb.a");
+	build.compile("usb-1.0");
 
 	Ok(())
 }
 
-#[cfg(target_os = "macos")]
-fn build() -> io::Result<()> {
+#[cfg(target_os = "linux")]
+fn build_hidapi() -> io::Result<()> {
 	let mut build = cc::Build::new();
 
-	build.file(source().join("libusb/hid.c"));
-	build.include(source().join("hidapi"));
-	build.static_flag(true);
+	build.file(source("hidapi").join("libusb/hid.c"));
+	build.include(source("hidapi").join("hidapi"));
+	build.include(source(LIBUSB_DIR).join("libusb"));
+	build.shared_flag(true);
 
-	for path in pkg_config::find_library("libusb-1.0").unwrap().include_paths {
-		build.include(path.to_str().unwrap());
-	}
-
-	build.compile("libhidapi.a");
+	build.compile("hidapi-libusb");
 
 	Ok(())
 }
 
-#[cfg(target_os = "windows")]
-fn build() -> io::Result<()> {
-	let mut build = cc::Build::new();
-
-	build.file(source().join("windows/hid.c"));
-	build.include(source().join("hidapi"));
-	build.static_flag(true);
-
-	build.compile("libhidapi.a");
-
-	Ok(())
-}
+//#[cfg(target_os = "macos")]
+//fn build() -> io::Result<()> {
+//	let mut build = cc::Build::new();
+//
+//	build.file(source("hidapi").join("libusb/hid.c"));
+//	build.include(source("hidapi").join("hidapi"));
+//	build.shared_flag(true);
+//
+//	for path in pkg_config::find_library("libusb-1.0").unwrap().include_paths {
+//		build.include(path.to_str().unwrap());
+//	}
+//
+//	build.compile("libhidapi.a");
+//
+//	Ok(())
+//}
+//
+//#[cfg(target_os = "windows")]
+//fn build() -> io::Result<()> {
+//	let mut build = cc::Build::new();
+//
+//	build.file(source("hidapi").join("windows/hid.c"));
+//	build.include(source("hidapi").join("hidapi"));
+//	build.shared_flag(true);
+//
+//	build.compile("libhidapi.a");
+//
+//	Ok(())
+//}
